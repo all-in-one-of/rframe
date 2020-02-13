0bc8829adbfb4082bbe9fc7b2c77ecb2cadabf2c
rf <rf@localhost.localdomain>
2020-02-14 00:26:21 +0100

    running docker

--- Docker/centos_image_devel/run.sh
+++ Docker/centos_image_devel/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -it   -p 8888:8888 rf/centos_image_devel_v003
+docker run -it -p 8888:8888 \
+--mount type=bind,source=$(dirname "$(dirname `pwd`)"),target=/env2/Projects/rFrame/rframe \
+ rf/centos_image_devel_v003
